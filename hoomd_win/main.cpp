1f5fc27629a46c9e1429b4a13269640bf651d262
Bin Wang <binwang.0213@gmail.com>
2020-01-31 21:32:00 -0600

    [Test] test_messenger.cc - GOOD

--- hoomd_win/main.cpp
+++ hoomd_win/main.cpp
@@ -1,346 +1,233 @@
+// Copyright (c) 2009-2019 The Regents of the University of Michigan
+// This file is part of the HOOMD-blue project, released under the BSD 3-Clause License.
+
+
 // this include is necessary to get MPI included before anything else to support intel MPI
 #include "ExecutionConfiguration.h"
 
 #include <iostream>
-
+#include <math.h>
 #include <memory>
 
-#include "GlobalArray.h"
-#include "GPUVector.h"
+#include <hoomd/extern/pybind/include/pybind11/pybind11.h>
 
-#ifdef ENABLE_CUDA
-#include "test_gpu_array.cuh"
-#endif
+#include "VectorMath.h"
 
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include <catch2/catch.hpp>
 
 using namespace std;
 
 
+/*! \file test_messenger.cc
+	\brief Unit test for Messenger
+	\ingroup unit_tests
+*/
 
-//! test case for testing the basic operation of GlobalArray
-TEST_CASE("GlobalArray_basic_tests")
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include <catch2/catch.hpp>
+
+//! Tolerance setting for near-zero comparisons
+const Scalar tol_small = Scalar(1e-3);
+
+//! Tolerance setting for comparisons
+const Scalar tol = Scalar(1e-2);
+
+//! Loose tolerance to be used with randomly generated and unpredictable comparisons
+Scalar loose_tol = Scalar(10);
+
+
+TEST_CASE("construction")
 {
-	std::shared_ptr<ExecutionConfiguration> exec_conf(new ExecutionConfiguration(ExecutionConfiguration::CPU));
-	GlobalArray<int> gpu_array(100, exec_conf);
+	// test each constructor separately
+	vec2<Scalar> a;
+	REQUIRE(std::abs(a.x) < tol_small);
+	REQUIRE(std::abs(a.y) < tol_small);
 
-	// basic check: ensure that the number of elements is set correctly
-	REQUIRE((int)gpu_array.getNumElements() == 100);
-
-	// basic check 2: acquire the data on the host and fill out a pattern
-	{
-		ArrayHandle<int> h_handle(gpu_array, access_location::host, access_mode::readwrite);
-		REQUIRE(h_handle.data != NULL);
-		for (int i = 0; i < (int)gpu_array.getNumElements(); i++)
-			h_handle.data[i] = i;
-	}
-
-	// basic check 3: verify the data set in check 2
-	{
-		ArrayHandle<int> h_handle(gpu_array, access_location::host, access_mode::read);
-		REQUIRE(h_handle.data != NULL);
-		for (int i = 0; i < (int)gpu_array.getNumElements(); i++)
-			REQUIRE(h_handle.data[i] == i);
-	}
-
-	// basic check 3.5: test the construction of a 2-D GlobalArray
-	GlobalArray<int> gpu_array_2d(63, 120, exec_conf);
-	REQUIRE((int)gpu_array_2d.getPitch() == 64);
-	REQUIRE((int)gpu_array_2d.getHeight() == 120);
-	REQUIRE((int)gpu_array_2d.getNumElements() == 7680);
-
-	// basic check 4: verify the copy constructor
-	GlobalArray<int> array_b(gpu_array);
-	REQUIRE((int)array_b.getNumElements() == 100);
-	REQUIRE((int)array_b.getPitch() == 100);
-	REQUIRE((int)array_b.getHeight() == 1);
-
-	{
-		ArrayHandle<int> h_handle(array_b, access_location::host, access_mode::read);
-		REQUIRE(h_handle.data != NULL);
-		for (int i = 0; i < (int)array_b.getNumElements(); i++)
-			REQUIRE(h_handle.data[i] == i);
-	}
-
-	// basic check 5: verify the = operator
-	GlobalArray<int> array_c(1, exec_conf);
-	array_c = gpu_array;
-
-	REQUIRE((int)array_c.getNumElements() == 100);
-	REQUIRE((int)array_c.getPitch() == 100);
-	REQUIRE((int)array_c.getHeight() == 1);
-
-	{
-		ArrayHandle<int> h_handle(array_c, access_location::host, access_mode::read);
-		REQUIRE(h_handle.data != NULL);
-		for (int i = 0; i < (int)array_c.getNumElements(); i++)
-			REQUIRE(h_handle.data[i] == i);
-	}
-
+	vec2<Scalar> b(123, 86);
+	REQUIRE(b.x == Approx(123));
+	REQUIRE(b.y == Approx(86));
 }
 
-#ifdef ENABLE_CUDA
-//! test case for testing device to/from host transfers
-TEST_CASE("GlobalArray_transfer_tests")
+TEST_CASE("component_wise")
 {
-	std::shared_ptr<ExecutionConfiguration> exec_conf(new ExecutionConfiguration(ExecutionConfiguration::GPU));
-	REQUIRE(exec_conf->isCUDAEnabled());
+	vec2<Scalar> a(1, 2);
+	vec2<Scalar> b(4, 6);
+	vec2<Scalar> c;
 
-	GlobalArray<int> gpu_array(100, exec_conf);
+	// test each component-wise operator separately
+	c = a + b;
+	REQUIRE(c.x == Approx(5));
+	REQUIRE(c.y == Approx(8));
 
-	// initialize the data on the device
-	{
-		ArrayHandle<int> d_handle(gpu_array, access_location::device, access_mode::readwrite);
-		REQUIRE(d_handle.data != NULL);
+	c = a - b;
+	REQUIRE(c.x == Approx(-3));
+	REQUIRE(c.y == Approx(-4));
 
-		gpu_fill_test_pattern(d_handle.data, gpu_array.getNumElements());
-		cudaError_t err_sync = cudaGetLastError();
-		exec_conf->handleCUDAError(err_sync, __FILE__, __LINE__);
-	}
+	c = a * b;
+	REQUIRE(c.x == Approx(4));
+	REQUIRE(c.y == Approx(12));
 
-	// copy it to the host and verify
-	{
-		ArrayHandle<int> h_handle(gpu_array, access_location::host, access_mode::readwrite);
-		REQUIRE(h_handle.data != NULL);
-		for (int i = 0; i < (int)gpu_array.getNumElements(); i++)
-		{
-			REQUIRE(h_handle.data[i] == i*i);
-			// overwrite the data as we verify
-			h_handle.data[i] = 100 + i;
-		}
-	}
+	c = a / b;
+	REQUIRE(c.x == Approx(1.0 / 4.0));
+	REQUIRE(c.y == Approx(2.0 / 6.0));
 
-	// test host-> device copies
-	{
-		ArrayHandle<int> d_handle(gpu_array, access_location::device, access_mode::readwrite);
-		REQUIRE(d_handle.data != NULL);
-
-		gpu_add_one(d_handle.data, gpu_array.getNumElements());
-		cudaError_t err_sync = cudaGetLastError();
-		exec_conf->handleCUDAError(err_sync, __FILE__, __LINE__);
-	}
-
-	// via the read access mode
-	{
-		ArrayHandle<int> h_handle(gpu_array, access_location::host, access_mode::read);
-		REQUIRE(h_handle.data != NULL);
-		for (int i = 0; i < (int)gpu_array.getNumElements(); i++)
-		{
-			REQUIRE(h_handle.data[i] == 100 + i + 1);
-		}
-	}
-
-	{
-		ArrayHandle<int> d_handle(gpu_array, access_location::device, access_mode::readwrite);
-		REQUIRE(d_handle.data != NULL);
-
-		gpu_add_one(d_handle.data, gpu_array.getNumElements());
-		cudaError_t err_sync = cudaGetLastError();
-		exec_conf->handleCUDAError(err_sync, __FILE__, __LINE__);
-	}
-
-	// and via the readwrite access mode
-	{
-		ArrayHandle<int> h_handle(gpu_array, access_location::host, access_mode::readwrite);
-		REQUIRE(h_handle.data != NULL);
-		for (int i = 0; i < (int)gpu_array.getNumElements(); i++)
-		{
-			REQUIRE(h_handle.data[i] == 100 + i + 1 + 1);
-		}
-	}
+	c = -a;
+	REQUIRE(c.x == Approx(-1));
+	REQUIRE(c.y == Approx(-2));
 }
 
-//! Tests operations on NULL GlobalArrays
-TEST_CASE("GlobalArray_null_tests")
+TEST_CASE("assignment_component_wise")
 {
-	// Construct a NULL GlobalArray
-	GlobalArray<int> a;
+	vec2<Scalar> a(1, 2);
+	vec2<Scalar> b(4, 6);
+	vec2<Scalar> c;
 
-	REQUIRE(a.isNull());
-	REQUIRE(a.getNumElements() == (unsigned)0);
+	// test each component-wise operator separately
+	c = a += b;
+	REQUIRE(c.x == Approx(5));
+	REQUIRE(c.y == Approx(8));
+	REQUIRE(a.x == Approx(5));
+	REQUIRE(a.y == Approx(8));
 
-	// check copy construction of a NULL GlobalArray
-	GlobalArray<int> b(a);
+	a = vec2<Scalar>(1, 2);
+	c = a -= b;
+	REQUIRE(c.x == Approx(-3));
+	REQUIRE(c.y == Approx(-4));
+	REQUIRE(a.x == Approx(-3));
+	REQUIRE(a.y == Approx(-4));
 
-	REQUIRE(b.isNull());
-	REQUIRE(b.getNumElements() == (unsigned)0);
+	a = vec2<Scalar>(1, 2);
+	c = a *= b;
+	REQUIRE(c.x == Approx(4));
+	REQUIRE(c.y == Approx(12));
+	REQUIRE(a.x == Approx(4));
+	REQUIRE(a.y == Approx(12));
 
-	// check assignment of a NULL GlobalArray
-	std::shared_ptr<ExecutionConfiguration> exec_conf(new ExecutionConfiguration(ExecutionConfiguration::GPU));
-	GlobalArray<int> c(1000, exec_conf);
-	c = a;
-
-	REQUIRE(c.isNull());
-	REQUIRE(c.getNumElements() == (unsigned)0);
-
-	// check swapping of a NULL GlobalArray
-	GlobalArray<int> d(1000, exec_conf);
-
-	d.swap(a);
-	REQUIRE(d.isNull());
-	REQUIRE(d.getNumElements() == (unsigned)0);
-
-	REQUIRE(!a.isNull());
-	REQUIRE(a.getNumElements() == (unsigned)1000);
+	a = vec2<Scalar>(1, 2);
+	c = a /= b;
+	REQUIRE(c.x == Approx(1.0 / 4.0));
+	REQUIRE(c.y == Approx(2.0 / 6.0));
+	REQUIRE(a.x == Approx(1.0 / 4.0));
+	REQUIRE(a.y == Approx(2.0 / 6.0));
 }
 
-//! Tests resize methods
-TEST_CASE("GlobalArray_resize_tests")
+TEST_CASE("scalar")
 {
-	std::shared_ptr<ExecutionConfiguration> exec_conf(new ExecutionConfiguration(ExecutionConfiguration::GPU));
+	vec2<Scalar> a(1, 2);
+	Scalar b(4);
+	vec2<Scalar> c;
 
-	// create a 1D GlobalArray
-	GlobalArray<unsigned int> a(5, exec_conf);
+	// test each component-wise operator separately
+	c = a * b;
+	REQUIRE(c.x == Approx(4));
+	REQUIRE(c.y == Approx(8));
 
-	{
-		// Fill it with some values
-		ArrayHandle<unsigned int> h_handle(a, access_location::host, access_mode::overwrite);
+	c = b * a;
+	REQUIRE(c.x == Approx(4));
+	REQUIRE(c.y == Approx(8));
 
-		h_handle.data[0] = 1;
-		h_handle.data[1] = 2;
-		h_handle.data[2] = 3;
-		h_handle.data[3] = 4;
-		h_handle.data[4] = 5;
-	}
-
-	// resize the array
-	a.resize(10);
-
-	// check that it has the right size
-	REQUIRE(a.getNumElements()== (unsigned int)10);
-	{
-		// test that it still contains the data
-		ArrayHandle<unsigned int> h_handle(a, access_location::host, access_mode::read);
-
-		REQUIRE(h_handle.data[0]== (unsigned int)1);
-		REQUIRE(h_handle.data[1]== (unsigned int)2);
-		REQUIRE(h_handle.data[2]== (unsigned int)3);
-		REQUIRE(h_handle.data[3]== (unsigned int)4);
-		REQUIRE(h_handle.data[4]== (unsigned int)5);
-
-		// test that the other elements are set to zero
-		REQUIRE(h_handle.data[5]== (unsigned int)0);
-		REQUIRE(h_handle.data[6]== (unsigned int)0);
-		REQUIRE(h_handle.data[7]== (unsigned int)0);
-		REQUIRE(h_handle.data[8]== (unsigned int)0);
-		REQUIRE(h_handle.data[9]== (unsigned int)0);
-	}
-
-	// check that it also works for a GlobalArray that is initially empty
-	GlobalArray<unsigned int> b;
-	b.resize(7);
-
-	REQUIRE(b.getNumElements()== (unsigned int)7);
-
-	// allocate a 2D GlobalArray
-	unsigned int width = 3;
-	unsigned int height = 2;
-	GlobalArray<unsigned int> c(width, height, exec_conf);
-	unsigned int pitch = c.getPitch();
-	{
-		// write some data to it
-		ArrayHandle<unsigned int> h_handle(c, access_location::host, access_mode::overwrite);
-
-		h_handle.data[0] = 123;
-		h_handle.data[1] = 456;
-		h_handle.data[2] = 789;
-
-		h_handle.data[0 + pitch] = 1234;
-		h_handle.data[1 + pitch] = 3456;
-		h_handle.data[2 + pitch] = 5678;
-	}
-
-	// resize it
-	width = 17;
-	height = 4;
-	c.resize(width, height);
-	pitch = c.getPitch();
-
-	// check that it has the right size
-	REQUIRE(c.getNumElements() == pitch*height);
-
-	{
-		// test that we can still recover the data
-		ArrayHandle<unsigned int> h_handle(c, access_location::host, access_mode::read);
-
-		REQUIRE(h_handle.data[0]== (unsigned int)123);
-		REQUIRE(h_handle.data[1]== (unsigned int)456);
-		REQUIRE(h_handle.data[2]== (unsigned int)789);
-
-		REQUIRE(h_handle.data[0 + pitch]== (unsigned int)1234);
-		REQUIRE(h_handle.data[1 + pitch]== (unsigned int)3456);
-		REQUIRE(h_handle.data[2 + pitch]== (unsigned int)5678);
-	}
+	c = a / b;
+	REQUIRE(c.x == Approx(1.0 / 4.0));
+	REQUIRE(c.y == Approx(2.0 / 4.0));
 }
 
-//! Tests GPUVector
-TEST_CASE("GPUVector_basic_tests")
+TEST_CASE("assignment_scalar")
 {
-	std::shared_ptr<ExecutionConfiguration> exec_conf(new ExecutionConfiguration(ExecutionConfiguration::GPU));
+	vec2<Scalar> a(1, 2);
+	Scalar b(4);
 
-	// First create an empty GPUVector
-	GPUVector<unsigned int> vec(exec_conf);
+	// test each component-wise operator separately
+	a = vec2<Scalar>(1, 2);
+	a *= b;
+	REQUIRE(a.x == Approx(4));
+	REQUIRE(a.y == Approx(8));
 
-	// The size should be zero
-	REQUIRE(vec.size()== (unsigned int)0);
+	a = vec2<Scalar>(1, 2);
+	a /= b;
+	REQUIRE(a.x == Approx(1.0 / 4.0));
+	REQUIRE(a.y == Approx(2.0 / 4.0));
+}
 
-	// Add some elements
-	vec.push_back(1);
-	vec.push_back(2);
-	vec.push_back(3);
+TEST_CASE("vector_ops")
+{
+	vec2<Scalar> a(1, 2);
+	vec2<Scalar> b(6, 5);
+	vec2<Scalar> c;
+	Scalar d;
 
-	REQUIRE(vec.size()== (unsigned int)3);
+	// test each vector operation
+	d = dot(a, b);
+	REQUIRE(d == Approx(16));
 
-	// Test bracket operator
-	REQUIRE((unsigned int)vec[0]== (unsigned int)1);
-	REQUIRE((unsigned int)vec[1]== (unsigned int)2);
-	REQUIRE((unsigned int)vec[2]== (unsigned int)3);
+	c = perp(a);
+	REQUIRE(std::abs(dot(a, c)) < tol_small);
+}
 
+TEST_CASE("comparison")
+{
+	vec2<Scalar> a(1.1, 2.1);
+	vec2<Scalar> b = a;
+	vec2<Scalar> c(2.1, 1.1);
+
+	// test equality
+	REQUIRE(a == b);
+	REQUIRE(!(a == c));
+
+	// test inequality
+	REQUIRE(!(a != b));
+	REQUIRE(a != c);
+}
+
+TEST_CASE("projection")
+{
+	vec2<Scalar> a(3.4, 5.5);
+	vec2<Scalar> b(0.1, 0);
+	vec2<Scalar> c;
+
+	// test projection
+	c = project(a, b);
+	REQUIRE(c.x == Approx(3.4));
+	REQUIRE(std::abs(c.y) < tol_small);
+}
+
+TEST_CASE("perpdot_product")
+{
+	vec2<Scalar> a(3.4, 5.5);
+	vec2<Scalar> b(0.1, -4.2);
+
+	// test projection
+	Scalar c = perpdot(a, b);
+	REQUIRE(c == Approx(-14.83));
+}
+
+TEST_CASE("test_swap")
+{
+	vec2<Scalar> a(1.1, 2.2);
+	vec2<Scalar> b(3.3, 4.4);
+	vec2<Scalar> c(1.1, 2.2);
+	vec2<Scalar> d(3.3, 4.4);
+
+	// test swap
+	a.swap(b);
+	REQUIRE(a == d);
+	REQUIRE(b == c);
+}
+
+TEST_CASE("test_assignment")
+{
 	// Test assignment
-	vec[1] = 4;
-
-	REQUIRE((unsigned int)vec[0]== (unsigned int)1);
-	REQUIRE((unsigned int)vec[1]== (unsigned int)4);
-	REQUIRE((unsigned int)vec[2]== (unsigned int)3);
-
-	// remove an element
-	vec.pop_back();
-
-	REQUIRE(vec.size()== (unsigned int)2);
-
-	// clear the array
-	vec.clear();
-
-	REQUIRE(vec.size()== (unsigned int)0);
-
-	// resize it
-	vec.resize(10);
-	REQUIRE(vec.size()== (unsigned int)10);
-
-	// verify we can still write to it
-	vec[0] = 234;
-	vec[1] = 123;
-	vec[2] = 654;
-	vec[3] = 789;
-	vec[4] = 321;
-	vec[5] = 432;
-	vec[6] = 543;
-	vec[7] = 678;
-	vec[8] = 987;
-	vec[9] = 890;
-
-	// read back
-	REQUIRE((unsigned int)vec[0]== (unsigned int)234);
-	REQUIRE((unsigned int)vec[1]== (unsigned int)123);
-	REQUIRE((unsigned int)vec[2]== (unsigned int)654);
-	REQUIRE((unsigned int)vec[3]== (unsigned int)789);
-	REQUIRE((unsigned int)vec[4]== (unsigned int)321);
-	REQUIRE((unsigned int)vec[5]== (unsigned int)432);
-	REQUIRE((unsigned int)vec[6]== (unsigned int)543);
-	REQUIRE((unsigned int)vec[7]== (unsigned int)678);
-	REQUIRE((unsigned int)vec[8]== (unsigned int)987);
-	REQUIRE((unsigned int)vec[9]== (unsigned int)890);
+	vec2<float> g;
+	vec2<double> h;
+	g = vec2<float>(121, 12);
+	REQUIRE(g.x == Approx(121));
+	REQUIRE(g.y == Approx(12));
+	g = vec2<double>(-122, 15);
+	REQUIRE(g.x == Approx(-122));
+	REQUIRE(g.y == Approx(15));
+	h = vec2<float>(18, 12);
+	REQUIRE(h.x == Approx(18));
+	REQUIRE(h.y == Approx(12));
+	h = vec2<double>(55, -64);
+	REQUIRE(h.x == Approx(55));
+	REQUIRE(h.y == Approx(-64));
 }
-#endif
-
---
+++ test/test_messenger.cc
@@ -0,0 +1,175 @@
+// Copyright (c) 2009-2019 The Regents of the University of Michigan
+// This file is part of the HOOMD-blue project, released under the BSD 3-Clause License.
+
+
+// this include is necessary to get MPI included before anything else to support intel MPI
+#include "ExecutionConfiguration.h"
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+#include "Messenger.h"
+
+using namespace std;
+
+
+/*! \file test_messenger.cc
+	\brief Unit test for Messenger
+	\ingroup unit_tests
+*/
+
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include <catch2/catch.hpp>
+
+
+TEST_CASE("Messenger_basic")
+{
+	Messenger msg;
+
+	// check that the default streams are set as documented
+	REQUIRE(&(msg.error())== &cerr);
+	REQUIRE(&(msg.warning())== &cerr);
+	REQUIRE(msg.getNoticeLevel() == (unsigned int)2);
+	REQUIRE(&(msg.notice(0))== &cout);
+	REQUIRE(&(msg.notice(1))== &cout);
+	REQUIRE(&(msg.notice(2))== &cout);
+	REQUIRE(&(msg.notice(3))== &(msg.getNullStream()));
+	cout << endl;
+
+	msg.setNoticeLevel(10);
+	REQUIRE(msg.getNoticeLevel() == (unsigned int)10);
+}
+
+
+TEST_CASE("Messenger_print")
+{
+	Messenger msg;
+
+	// try a few test messages (must be determined by hand runs)
+	msg.error() << "This is an error message test " << 1 << " " << 786.345 << endl;
+	msg.warning() << "This is a warning message test" << endl;
+	msg.notice(9) << "This notice message should not be printed" << endl;
+	msg.setNoticeLevel(10);
+	msg.notice(9) << "And this one should" << endl;
+	msg.notice(1) << "Notice level 1" << endl;
+}
+
+TEST_CASE("Messenger_null")
+{
+	Messenger msg;
+
+	// try setting the null streams
+	msg.error() << "This message should be printed" << endl;
+	msg.setErrorStream(msg.getNullStream());
+	msg.error() << "But this one should not" << endl;
+
+	msg.warning() << "This message should be printed" << endl;
+	msg.setWarningStream(msg.getNullStream());
+	msg.warning() << "But this one should not" << endl;
+
+	msg.notice(1) << "This message should be printed" << endl;
+	msg.setNoticeStream(msg.getNullStream());
+	msg.notice(1) << "But this one should not" << endl;
+}
+
+TEST_CASE("Messenger_prefix")
+{
+	Messenger msg;
+
+	// check that set/get work on prefixes
+	msg.setErrorPrefix("err");
+	REQUIRE(msg.getErrorPrefix()== string("err"));
+	msg.setWarningPrefix("warn");
+	REQUIRE(msg.getWarningPrefix()== string("warn"));
+	msg.setNoticePrefix("note");
+	REQUIRE(msg.getNoticePrefix()== string("note"));
+
+	ostringstream strm;
+	// check that the prefixes are used properly
+	msg.setErrorStream(strm);
+	msg.error() << "message";
+	REQUIRE(strm.str()== string("err: message"));
+
+	strm.str("");
+	msg.setWarningStream(strm);
+	msg.warning() << "message";
+	REQUIRE(strm.str()== string("warn: message"));
+
+	strm.str("");
+	msg.setNoticeStream(strm);
+	msg.notice(1) << "message";
+	REQUIRE(strm.str()== string("message"));
+
+	strm.str("");
+	msg.setNoticeStream(strm);
+	msg.notice(5) << "message";
+	REQUIRE(strm.str()== string(""));
+	msg.setNoticeLevel(5);
+	msg.notice(5) << "message";
+	REQUIRE(strm.str()== string("note(5): message"));
+
+	// try copying a messenger and make sure that it still works
+	strm.str("");
+	Messenger msg2;
+	msg2 = msg;
+	msg2.error() << "1" << endl;
+	msg.warning() << "2" << endl; // intentional use of msg to see if it merges output as it should
+	msg2.notice(1) << "3" << endl;
+	msg2.notice(5) << "4" << endl;
+	msg2.notice(6) << "5" << endl;
+	REQUIRE(strm.str()== string("err: 1\nwarn: 2\n3\nnote(5): 4\n"));
+}
+
+TEST_CASE("Messenger_file")
+{
+	// scope the messengers so that the file is closed and written
+	{
+		Messenger msg;
+
+		msg.setErrorPrefix("err");
+		msg.setWarningPrefix("warn");
+		msg.setNoticePrefix("note");
+
+		// test opening a file
+		msg.openFile("test_messenger_output");
+
+		// also test that we can write to the file from 2 messengers
+		Messenger msg2(msg);
+		msg2.setNoticeLevel(5);
+
+		// write the file (also tests the *Str functions)
+		msg.errorStr("Error 1\n");
+		msg2.warningStr("Warning 2\n");
+		msg.noticeStr(1, "Notice 3\n");
+		msg.noticeStr(5, "This shouldn't be printed\n");
+		msg2.noticeStr(5, "Notice 4\n");
+	}
+
+	// make sure the file was created
+	//REQUIRE(filesystem::exists("test_messenger_output"));
+
+	// read in the file and make sure correct data was written
+	ifstream f("test_messenger_output");
+	string line;
+
+	getline(f, line);
+	REQUIRE(line == "err: Error 1");
+	REQUIRE(!f.bad());
+
+	getline(f, line);
+	REQUIRE(line == "warn: Warning 2");
+	REQUIRE(!f.bad());
+
+	getline(f, line);
+	REQUIRE(line == "Notice 3");
+	REQUIRE(!f.bad());
+
+	getline(f, line);
+	REQUIRE(line == "note(5): Notice 4");
+	REQUIRE(!f.bad());
+	f.close();
+
+	unlink("test_messenger_output");
+}
